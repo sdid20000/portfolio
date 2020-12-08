5687183a14921f0f468a8de9c17b69f07ddcc226
sdid20000 <michaelthientu@gmail.com>
2020-12-08 14:42:45 -0500

    Capitalization

--- src/Resume.js
+++ src/Resume.js
@@ -14,7 +14,7 @@ class Resume extends React.Component {
         <h1 class="text-center mt-5 font-weight-bold">Resum&eacute;</h1>
         <h4 class="text-center grey-text">Updated as of Dec. 8, 2020</h4>
         <img
-          src="/images/resume/resume.png"
+          src="/images/Resume/resume.png"
           className="mx-auto d-block w-50 border shadow rounded"
         ></img>
       </div>
